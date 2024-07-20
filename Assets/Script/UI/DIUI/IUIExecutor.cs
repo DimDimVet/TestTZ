f77fe65ba0e7f7f19fdeb8353afe04212409eb55
ДимДим <1000rub@list.ru>
2024-07-20 12:21:09 +0300

    r200724

--- Assets/Script/UI/DIUI/IUIExecutor.cs
+++ Assets/Script/UI/DIUI/IUIExecutor.cs
@@ -5,8 +5,8 @@ namespace UI
 {
     public interface IUIExecutor
     {
-        Action OnInventary { get; set; }
-        void Inventary(StatusCustomButton _status);
+        //Action OnInventary { get; set; }
+        //void Inventary(StatusCustomButton _status);
         //Action<int, int, DropData> OnSetCollisionHash { get; set; }
         //void SetReturnData(int _thisHash, int _receptionHash, DropData _dropData);
     }
--- Assets/Script/UI/DIUI/UIExecutor.cs
+++ Assets/Script/UI/DIUI/UIExecutor.cs
@@ -4,30 +4,30 @@ namespace UI
 {
     public class UIExecutor : IUIExecutor
     {
-        public Action OnInventary { get { return onInventary; } set { onInventary = value; } }
-        private Action onInventary;
+        //public Action OnInventary { get { return onInventary; } set { onInventary = value; } }
+        //private Action onInventary;
 
         //public void SetReturnData(int _thisHash, int _receptionHash, DropData _dropData)
         //{
         //    onSetCollisionHash?.Invoke(_thisHash, _receptionHash, _dropData);
         //}
 
-        public void Inventary(StatusCustomButton _status)
-        {
-            switch (_status)
-            {
-                case StatusCustomButton.PointDown:
-                    onInventary?.Invoke();
-                    break;
-                case StatusCustomButton.PointUp:
+        //public void Inventary(StatusCustomButton _status)
+        //{
+        //    switch (_status)
+        //    {
+        //        case StatusCustomButton.PointDown:
+        //            onInventary?.Invoke();
+        //            break;
+        //        case StatusCustomButton.PointUp:
 
-                    break;
+        //            break;
 
-                default:
+        //        default:
 
-                    break;
-            }
-        }
+        //            break;
+        //    }
+        //}
     }
 }
 
